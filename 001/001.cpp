b025a345effc449b021990118b3d38266f9683d2
nunayoii <4b3g0090@office.stust.edu.tw>
2024-11-13 19:52:32 +0800

    1

--- 001/001.cpp
+++ 001/001.cpp
@@ -5,7 +5,14 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello World!\n";
+    int n,sum=0;
+    cout << "請輸入一個正整數n:";
+    cin >> n;
+
+    for (int i = 0; i <= n; i += 2) {
+        sum += i;
+    }
+    cout << "偶數總和為:" << sum;
 }
 
 
