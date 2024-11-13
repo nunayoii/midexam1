2c92b263cf0944410e971ed80fdd56366a2518bd
nunayoii <4b3g0090@office.stust.edu.tw>
2024-11-13 15:31:37 +0800

    2

--- 001/001.cpp
+++ 001/001.cpp
@@ -9,21 +9,20 @@ int main()
     cout << "輸入月份:";
     cin >> m;
 
-    switch (m) {
-        case 3: case 4: case 5:
-            cout << "春季";
-            break;
-        case 6: case 7: case 8:
-            cout << "夏季";
-            break;
-        case 9: case 10: case 11:
-            cout << "秋季";
-            break;
-        case 12: case 1: case 2:
-            cout << "冬季";
-            break;
-        default:
-            cout << "輸入正確的月份";
+    if (m >= 3 && m <= 5) {
+        cout << "春季";
+    }
+    else if (m >= 6 && m <= 8) {
+        cout << "夏季";
+    }
+    else if (m >= 9 && m <= 11) {
+        cout << "秋季";
+    }
+    else if (m == 12 || m == 1 || m == 2) {
+        cout << "冬季";
+    }
+    else {
+        cout << "!!輸入正確月份!!";
     }
     return 0;
 }
