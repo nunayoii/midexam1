64fd0a3c1e3ffe5110a3c28655f410b95086da23
nunayoii <4b3g0090@office.stust.edu.tw>
2024-11-13 20:03:18 +0800

    2

--- 001/001.cpp
+++ 001/001.cpp
@@ -5,12 +5,16 @@ using namespace std;
 
 int main()
 {
-    int n,sum=0;
+    int n;
     cout << "請輸入一個正整數n:";
     cin >> n;
 
-    for (int i = 0; i <= n; i += 2) {
+    int sum = 0;
+    int i = 0;
+
+    while (i <= n) {
         sum += i;
+        i += 2;
     }
     cout << "偶數總和為:" << sum;
 }
